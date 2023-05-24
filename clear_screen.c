b99c7137a986a6847ff9d82cd8a0b8d0ef996605
Leo Hsu <leo@LeodeMacBook-Pro.local>
2023-05-24 13:58:45 +0800

    add some program

--- clear_screen.c
+++ clear_screen.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-    //system("cls");
+    //system("cls"); // for windows
+    //system("clear"); // for mac
     printf("\033[H\033[2J");
     printf("hello world\n");
 }
