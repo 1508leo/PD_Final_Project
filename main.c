c71b9954119d1e1e7f2edfb76769c018d79796af
Leo Hsu <leo@LeodeMacBook-Pro.local>
2023-05-24 21:34:12 +0800

    command to run sdl2

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include "total.h"
 
+/* gcc test.c -I/Library/Frameworks/SDL2.framework/Headers -F/Library/Frameworks -framework SDL2 -F/Library/Frameworks -framework SDL2_ttf
+*/
+
 int number_ad = 4; //trace the amount of administrator initialize 4 administrator
 int amount_books = 0; // trace the amount of books
 int accession_numer = 0; //record accession_number
