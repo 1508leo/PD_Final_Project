f6ee073c98ed1811621ae6c322fd9fd007dff0e6
Leo Hsu <leo@LeodeMacBook-Pro.local>
2023-06-08 21:37:09 +0800

    delete sort call_number

--- sort.c
+++ sort.c
@@ -34,42 +34,6 @@ void sort_publish_year()
     
 }
 
-/* Need to change
-void sort_call_number()
-{
-    static int order = SMALL;
-
-    /* Large first 
-    for(int i = 0; i < amount_books - 1; i++)
-    {
-        for(int j = 0; j < amount_books - i; j++)
-        {
-            if(book[j].call_number < book[j + 1].call_number)
-            {
-                struct books temp = book[j];
-                book[j] = book[j + 1];
-                book[j + 1] = temp;
-            }
-        }
-    }
-
-    if(order == SMALL)
-        order = LARGE;
-    else if(order == LARGE)
-    {
-        order = SMALL;
-        /* Small first, reverse the structure 
-        for(int i = 0; i < (amount_books / 2); i++)
-        {
-            struct books temp = book[i];
-            book[i] = book[amount_books - i - 1];
-            book[amount_books - i - 1] = temp;
-        }
-    }
-    
-}
-*/
-
 void sort_viewer_number()
 {
     static int order = SMALL;
